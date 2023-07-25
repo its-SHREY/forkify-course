21eb0cc8507c27d46876adb99db1f030bb10c00a
its-SHREY <omeva.2010@gmail.com>
2023-07-25 07:57:13 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '90f0b22c-c49e-4a92-ab81-7f5e0e04a492';
+export const MODAL_CLOSE_SEC = 2.5;
