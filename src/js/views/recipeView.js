c922ce043bb1f687432e608761d7eafa4c833430
its-SHREY <omeva.2010@gmail.com>
2023-07-25 08:56:19 +0530

    Fractional Fix#2

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -129,7 +129,9 @@ class RecipeView extends View {
                 <use href="${icon}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                ing.quantity
+                  ? new numberToFraction(ing.quantity).toString()
+                  : ''
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
